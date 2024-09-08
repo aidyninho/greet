48250814953830ef67f6ccdfbdbe95ddd52507d0
aidyninho <ryzyk.aidyn@gmail.com>
2024-09-08 07:33:13 +0500

    Add UserService test

---
+++ src/main/java/com/aidyninho/greet/servlet/GetUserServlet.java
@@ -0,0 +1,21 @@
+package com.aidyninho.greet.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/users/*")
+public class GetUserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        Long id = Long.parseLong(req.getPathInfo().substring(1));
+
+
+    }
+}
---
+++ src/test/java/com/aidyninho/greet/service/UserDaoTest.java
@@ -0,0 +1,53 @@
+package com.aidyninho.greet.service;
+
+import com.aidyninho.greet.dao.UserDao;
+import com.aidyninho.greet.model.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserDao userDao;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void saveReturnsTrue() {
+        Mockito.doReturn(true).when(userDao).save(Mockito.any(User.class));
+        boolean actual = userService.save(new User(null, "John", "Doe"));
+        assertTrue(actual);
+    }
+
+    @Test
+    public void getByIdReturnsUser() {
+        User expected = new User(1L, "John", "Doe");
+        Mockito.doReturn(expected).when(userDao).getById(1L);
+        User actual = userService.findById(1L);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void updateReturnsTrue() {
+        Mockito.doReturn(true).when(userDao).update(Mockito.any(User.class));
+        boolean actual = userService.update(new User(null, "NotJohn", "NotDoe"));
+        assertTrue(actual);
+    }
+
+    @Test
+    public void deleteReturnsTrue() {
+        Mockito.doReturn(true).when(userDao).delete(1L);
+        boolean actual = userService.delete(1L);
+        assertTrue(actual);
+    }
+
+}
