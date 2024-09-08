cda45ac39d7d476fed649b1bbe1fb78c4340b80a
aidyninho <ryzyk.aidyn@gmail.com>
2024-09-09 00:09:34 +0500

    Add CRUD servlets

---
+++ src/main/java/com/aidyninho/greet/servlet/SaveAndUpdateUserServlet.java
@@ -0,0 +1,52 @@
+package com.aidyninho.greet.servlet;
+
+import com.aidyninho.greet.model.User;
+import com.aidyninho.greet.service.UserService;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/users")
+public class SaveAndUpdateUserServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json");
+        String firstname = req.getParameter("firstname");
+        String lastname = req.getParameter("lastname");
+
+        User user = new User(null, firstname, lastname);
+
+        UserService userService = UserService.getInstance();
+        boolean saved = userService.save(user);
+
+        if (saved) {
+            resp.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json");
+
+        Long id = Long.parseLong(req.getParameter("id"));
+        String firstname = req.getParameter("firstname");
+        String lastname = req.getParameter("lastname");
+
+        User user = new User(id, firstname, lastname);
+
+        UserService userService = UserService.getInstance();
+        boolean updated = userService.update(user);
+
+        if (updated) {
+            resp.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+}
